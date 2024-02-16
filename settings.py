a9c0af276fc44f52315d945bc27c43244bc424ba
K-Aberin <160270324+K-Aberin@users.noreply.github.com>
2024-02-16 10:20:18 -0800

    hello github

---
+++ settings.py
@@ -0,0 +1,22 @@
+#This file was created by: Kai Aberin
+TITLE = "i am very green but im mean"
+
+FPS = 30
+PLAYER_SPEED = 300
+
+#this is 32 grids wide
+WIDTH = 1024
+#this is 24 grids high
+HEIGHT = 768
+TILESIZE = 32
+
+BLACK = (0,0,0)
+LIGHTBLUE = (111,186,234)
+RED = (255,0,0)
+GREEN = (0,255,0)
+BLUE = (0,0,255)
+YELLOW = (255,255,0)
+SLIGHTLYLESSYELLOW = (225,225,0)
+LIGHTGREY = (128,128,128)
+
+BGCOLOR = LIGHTBLUE
